65a32cc8f93193ae77e93d9f659f971d3bf9138a
JRWaugh <joshua@waugh.zone>
2019-11-06 00:59:38 +0200

    Fixing ex04

--- Exercise04/Exercise04/Exercise04.cpp
+++ Exercise04/Exercise04/Exercise04.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 #include "String.h"
 int main() {
-	String s1("abcedf");
-	String s2("ghijklmn");
-	String sum;
-	std::cout << "String 1 is:" << s1 << std::endl;
-	std::cout << "String 2 is:" << s2 << std::endl;
-	sum = s1 + s2;
-	std::cout << "Sum is :" << sum << std::endl; // output should be
-	// abcdefghijklmn
-   // This should also work
-	sum = sum;
-	std::cout << "Sum is still :" << sum << std::endl; // output should
-	 // still be abcdefghijklmn
+	String s("abcdefg");
+	for (int i = 0; i < 7; i++)
+		std::cout << s[i] << " ";
+	s[4] = 'X';
+	for (int i = 0; i < 7; i++)
+		std::cout << s[i] << " ";
 	return 0;
 }
--- Exercise04/Exercise04/String.cpp
+++ Exercise04/Exercise04/String.cpp
@@ -1,24 +1,28 @@
 #include <algorithm>
 #include "String.h"
 
-String::String(char const *string) {
+String::String(char const* string) {
 	c_string = new char[strlen(string) + 1];
 	strcpy_s(c_string, strlen(string) + 1, string);
 }
 
-String::String(String const &string) {
+String::String(String const& string) {
 	c_string = new char[strlen(string.c_string) + 1];
 	strcpy_s(c_string, strlen(string.c_string) + 1, string.c_string);
 }
 
-String& String::operator = (String string) {
-	//Copied mostly from online because I realised I was writing a copy constructor/destructor twice.
-	std::swap(*this, string);
+const String& String::operator = (const String& string) {
+	if (this != &string) {
+		delete[] c_string;
+		c_string = new char[strlen(string.c_string) + 1];
+		strcpy_s(c_string, strlen(string.c_string) + 1, string.c_string);
+	}
 	return *this;
 }
 
-String String::operator + (String const &string) const {
+String String::operator + (String const& string) const {
 	String temp;
+	delete[] c_string;
 	temp.c_string = new char[strlen(c_string) + strlen(string.c_string) + 1];
 	strcpy_s(temp.c_string, strlen(c_string) + 1, c_string);
 	strcat_s(temp.c_string, strlen(c_string) + strlen(string.c_string) + 1, string.c_string);
@@ -33,7 +37,35 @@ void String::list() {
 	std::cout << c_string << std::endl;
 }
 
-std::ostream& operator << (std::ostream& os, String const &string) {
+std::ostream& operator << (std::ostream& os, String const& string) {
 	os << string.c_string;
 	return os;
+}
+
+String& String::operator++() {
+	char* temp = new char[strlen(c_string) + 2];
+	strcpy_s(temp, strlen(c_string) + 2, c_string);
+	strcat_s(temp, strlen(c_string) + 2, "X");
+	delete[] c_string;
+	c_string = temp;
+	return *this;
+}
+
+String String::operator++(int) {
+	String old(*this);
+	char* temp = new char[strlen(old.c_string) + 2];
+	strcpy_s(temp, strlen(old.c_string) + 2, old.c_string);
+	strcat_s(temp, strlen(old.c_string) + 2, "X");
+	delete[] c_string;
+	c_string = temp;
+	return old;
+}
+
+char& String::operator[](int i) {
+	if (i >= 0 && i < strlen(c_string))
+		return c_string[i];
+	else {
+		std::cout << "Index out of bounds" << std::endl;
+		return c_string[0];
+	}
 }
